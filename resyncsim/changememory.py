18f0eb84eb1cee7de09c566f37887ab5fca5f7b0
Bernhard Haslhofer <bernhard.haslhofer@cornell.edu>
2012-05-03 21:48:00 -0400

    implemented simple form of paging for change memory

--- resyncsim/changememory.py
+++ resyncsim/changememory.py
@@ -12,12 +12,15 @@ import tornado.web
 from observer import Observer
 
 class ChangeMemory(Observer):
-    """An change memory keeps track of changes"""
+    """An abstract change memory implementation that doesn't do anything.
+    ChangeMemory implementations can extend this class
+    """
     
     def __init__(self, source):
         self.source = source
         source.register_observer(self)
 
+# A dynamic in-memory change digest
 
 class DynamicDigest(ChangeMemory):
     """A change memory that stores changes in an in-memory list"""
@@ -47,5 +50,60 @@ class DynamicDigestHandler(tornado.web.RequestHandler):
     
     def get(self):
         self.set_header("Content-Type", "application/xml")
+        print self.changes
         self.render("change_digest.xml",
                     changes = self.changes)
+
+
+# A dynamic change digest that implements client-driven paging                    
+
+class DynamicPagingDigest(ChangeMemory):
+    """A change memory that stores changes in an in-memory list"""
+
+    def __init__(self, source, config):
+        super(DynamicPagingDigest, self).__init__(source)
+        self.url = config['url']
+        self.changes = []
+        
+    def notify(self, event):
+        """Simply store a change in the in-memory list"""
+        self.changes.append(event)
+    
+    @property
+    def handlers(self):
+        return [(r"/changes", 
+                DynamicPagingDigestHandler,
+                dict(changes = self.changes)),
+                (r"/changes/([0-9]+)/diff",
+                PartialDynamicPagingDigestHandler,
+                dict(changes = self.changes)),
+        ]
+        
+
+class DynamicPagingDigestHandler(tornado.web.RequestHandler):
+    """The HTTP request handler for the DynamicPagingDigest"""
+
+    def initialize(self, changes):
+        self.changes = changes
+
+    def get(self):
+        self.set_header("Content-Type", "application/xml")
+        self.render("change_digest_paging.xml",
+                    changes = self.changes)
+
+class PartialDynamicPagingDigestHandler(tornado.web.RequestHandler):
+    """The HTTP request handler for the DynamicPagingDigest"""
+
+    def initialize(self, changes):
+        self.changes = changes
+
+    def get(self, seqId):
+        self.set_header("Content-Type", "application/xml")
+        self.render("change_digest_paging_partial.xml",
+                    last_seq_id = int(seqId),
+                    changes = self.changes)
+
+
+
+
+        
