8edc434b1ea4a2b46715751d5c7d9bd72a25cc0e
Bernhard Haslhofer <bernhard.haslhofer@cornell.edu>
2012-05-25 19:38:33 -0400

    changed source's resource storage behavior

--- resync/source.py
+++ resync/source.py
@@ -9,6 +9,7 @@ Copyright 2012, ResourceSync.org. All rights reserved.
 
 import random
 import hashlib
+import pprint
 
 import time
 from datetime import datetime
@@ -26,7 +27,7 @@ class Source(Observable):
         super(Source, self).__init__()
         self.config = config
         self.max_res_id = 0
-        self.resources = {} # stores resources by id | resource_object
+        self._repository = {} # stores resources by {id | {timestamp | size} }
         self.bootstrap()
         
     def bootstrap(self):
@@ -39,12 +40,22 @@ class Source(Observable):
         for i in range(self.config['number_of_resources']):
             self.create_resource(notify_observers = False)
     
+    def resource(self, res_id):
+        """Generate a resource object from internal resource repository"""
+        res = Resource(
+            id = res_id,
+            size = self._repository[res_id]['size'],
+            lastmod = datetime.fromtimestamp(
+                            self._repository[res_id]['timestamp']).isoformat()
+        )
+        return res
     
     def random_resources(self, number = 1):
         "Return a random set of resources, at most all resources"
-        if number > len(self.resources):
-            number = len(self.resources)
-        return random.sample(self.resources.values(), number)
+        if number > len(self._repository):
+            number = len(self._repository)
+        rand_res_ids = random.sample(self._repository.keys(), number)
+        return [self.resource(res_id) for res_id in rand_res_ids]
 
     def random_resource(self):
         "Selects a single random resource"
@@ -54,33 +65,35 @@ class Source(Observable):
         else:
             raise "Unexpected empty result set when selecting random resource"
     
-    def create_resource(self, notify_observers = True):
+    def create_resource(self, res_id = None, notify_observers = True):
         """Create a new resource, add it to the source, notify observers."""
-        res_id = self.max_res_id
-        self.max_res_id += 1
-        res = Resource(
-            id = res_id,
-            size = random.randint(0, self.config['average_payload'])
-        )
-        self.resources[res_id] = res
+        if res_id == None:
+            res_id = self.max_res_id
+            self.max_res_id += 1
+        timestamp = time.time()
+        size = random.randint(0, self.config['average_payload'])
+        self._repository[res_id] = {'timestamp': timestamp, 'size': size}
+        res = self.resource(res_id)
         if notify_observers:
             event = ChangeEvent("CREATE", res)
             self.notify_observers(event)
+        return res
         
     def update_resource(self, res):
         """Update a resource, notify observers."""
-        res = self.resources[res.id]
-        res.update(random.randint(0, self.config['average_payload']))
+        self.delete_resource(res, notify_observers = False)
+        res = self.create_resource(res.id, notify_observers = False)
         event = ChangeEvent("UPDATE", res)
         self.notify_observers(event)
 
-    def delete_resource(self, res):
+    def delete_resource(self, res, notify_observers = True):
         """Delete a given resource, notify observers."""
-        res = self.resources[res.id]
-        del self.resources[res.id]
+        res = self.resource(res.id)
+        del self._repository[res.id]
         res.lastmod = datetime.now().isoformat('T')
-        event = ChangeEvent("DELETE", res)
-        self.notify_observers(event)
+        if notify_observers:
+            event = ChangeEvent("DELETE", res)
+            self.notify_observers(event)
     
     def simulate_changes(self):
         """Simulate changing resources in the source"""
@@ -114,14 +127,14 @@ class Source(Observable):
     
     def __str__(self):
         """Prints out the source's resources"""
-        return '\n'.join([str(r) for (id, r) in self.resources.items()])
+        return pprint.pformat(self._repository)
 
         
 # run standalone for testing purposes
 if __name__ == '__main__':
     config = dict(
         number_of_resources = 10,
-        change_frequency = 2,
+        change_frequency = 1,
         average_payload = 100,
         event_types = ['create', 'update', 'delete'],
         max_events = 5)
