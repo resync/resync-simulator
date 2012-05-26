434be3ff01fee93fbf37f57d9f2b523f4f334436
Bernhard Haslhofer <bernhard.haslhofer@cornell.edu>
2012-05-26 13:33:03 -0400

    marked private methods in source

--- resync/source.py
+++ resync/source.py
@@ -26,20 +26,12 @@ class Source(Observable):
         """Initalize the source"""
         super(Source, self).__init__()
         self.config = config
-        self.max_res_id = 0
+        self.max_res_id = 1
         self._repository = {} # {basename, {timestamp, size}}
-        self.bootstrap()
+        self._bootstrap()
         
-    def bootstrap(self):
-        """Bootstrap the source with a set of resources"""
-        print "*** Bootstrapping source with %d resources and an average " \
-                "resource payload of %d bytes ***" \
-                 % (self.config['number_of_resources'],
-                    self.config['average_payload'])
+    # Public Methods
         
-        for i in range(self.config['number_of_resources']):
-            self.create_resource(notify_observers = False)
-    
     @property
     def resource_count(self):
         """The number of resources in the repository"""
@@ -69,42 +61,6 @@ class Source(Observable):
             number = len(self._repository)
         rand_basenames = random.sample(self._repository.keys(), number)
         return [self.resource(basename) for basename in rand_basenames]
-
-    def random_resource(self):
-        "Selects a single random resource"
-        rand_res = self.random_resources(1)
-        if len(rand_res) == 1:
-            return rand_res[0]
-        else:
-            raise "Unexpected empty result set when selecting random resource"
-    
-    def create_resource(self, basename = None, notify_observers = True):
-        """Create a new resource, add it to the source, notify observers."""
-        if basename == None:
-            basename = str(self.max_res_id)
-            self.max_res_id += 1
-        timestamp = time.time()
-        size = random.randint(0, self.config['average_payload'])
-        self._repository[basename] = {'timestamp': timestamp, 'size': size}
-        if notify_observers:
-            event = ChangeEvent("CREATE", self.resource(basename))
-            self.notify_observers(event)
-        
-    def update_resource(self, basename):
-        """Update a resource, notify observers."""
-        self.delete_resource(basename, notify_observers = False)
-        res = self.create_resource(basename, notify_observers = False)
-        event = ChangeEvent("UPDATE", self.resource(basename))
-        self.notify_observers(event)
-
-    def delete_resource(self, basename, notify_observers = True):
-        """Delete a given resource, notify observers."""
-        res = self.resource(basename)
-        del self._repository[basename]
-        res.timestamp = time.time()
-        if notify_observers:
-            event = ChangeEvent("DELETE", res)
-            self.notify_observers(event)
     
     def simulate_changes(self):
         """Simulate changing resources in the source"""
@@ -118,7 +74,7 @@ class Source(Observable):
             time.sleep(sleep_time)
             event_type = random.choice(self.config['event_types'])
             if event_type == "create":
-                self.create_resource()
+                self._create_resource()
             elif event_type == "update" or event_type == "delete":
                 if len(self._repository.keys()) > 0:
                     basename = random.sample(self._repository.keys(), 1)[0]
@@ -129,16 +85,56 @@ class Source(Observable):
                     no_events = no_events + 1                    
                     continue
                 if event_type == "update":
-                    self.update_resource(basename)
+                    self._update_resource(basename)
                 elif event_type == "delete":
-                    self.delete_resource(basename)
-                    
+                    self._delete_resource(basename)
+
             else:
                 print "Event type %s is not supported" % event_type
             no_events = no_events + 1
-        
+
         print "*** Finished change simulation ***"
     
+    # Private Methods
+    
+    def _create_resource(self, basename = None, notify_observers = True):
+        """Create a new resource, add it to the source, notify observers."""
+        if basename == None:
+            basename = str(self.max_res_id)
+            self.max_res_id += 1
+        timestamp = time.time()
+        size = random.randint(0, self.config['average_payload'])
+        self._repository[basename] = {'timestamp': timestamp, 'size': size}
+        if notify_observers:
+            event = ChangeEvent("CREATE", self.resource(basename))
+            self.notify_observers(event)
+        
+    def _update_resource(self, basename):
+        """Update a resource, notify observers."""
+        self._delete_resource(basename, notify_observers = False)
+        self._create_resource(basename, notify_observers = False)
+        event = ChangeEvent("UPDATE", self.resource(basename))
+        self.notify_observers(event)
+
+    def _delete_resource(self, basename, notify_observers = True):
+        """Delete a given resource, notify observers."""
+        res = self.resource(basename)
+        del self._repository[basename]
+        res.timestamp = time.time()
+        if notify_observers:
+            event = ChangeEvent("DELETE", res)
+            self.notify_observers(event)
+    
+    def _bootstrap(self):
+        """Bootstrap the source with a set of resources"""
+        print "*** Bootstrapping source with %d resources and an average " \
+                "resource payload of %d bytes ***" \
+                 % (self.config['number_of_resources'],
+                    self.config['average_payload'])
+
+        for i in range(self.config['number_of_resources']):
+            self._create_resource(notify_observers = False)
+
     def __str__(self):
         """Prints out the source's resources"""
         return pprint.pformat(self._repository)
