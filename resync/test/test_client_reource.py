1cca2df818243be285899ccd06a37fcc3f4333d2
Simeon Warner <simeon.warner@cornell.edu>
2012-06-06 17:37:20 -0400

    Move lastmod setter into resync/resource.py

--- resync/test/test_client_reource.py
+++ resync/test/test_client_reource.py
@@ -20,11 +20,11 @@ class TestClientResource(unittest.TestCase):
     def test1c_same(self):
         """Same with lastmod instead of direct timestamp"""
         r1 = ClientResource('a')
-        r1.set_lastmod('2012-01-02')
+        r1.lastmod='2012-01-02'
         r2 = ClientResource('a')
-        r2.set_lastmod('2012-01-02T00:00:00')
+        r2.lastmod='2012-01-02T00:00:00'
         r3 = ClientResource('a')
-        r3.set_lastmod('2012-01-02T00:00:00.00')
+        r3.lastmod='2012-01-02T00:00:00.00'
         self.assertEqual( r1.timestamp, r2.timestamp )
         self.assertEqual( r1.timestamp, r3.timestamp )
         self.assertEqual( r1, r1 )
@@ -34,9 +34,9 @@ class TestClientResource(unittest.TestCase):
     def test1d_same(self):
         """Same with slight timestamp diff"""
         r1 = ClientResource('a')
-        r1.set_lastmod('2012-01-02T01:02:03')
+        r1.lastmod='2012-01-02T01:02:03'
         r2 = ClientResource('a')
-        r2.set_lastmod('2012-01-02T01:02:03.99')
+        r2.lastmod='2012-01-02T01:02:03.99'
         self.assertNotEqual( r1.timestamp, r2.timestamp )
         self.assertEqual( r1, r2 )
 
@@ -53,12 +53,24 @@ class TestClientResource(unittest.TestCase):
 
     def test3a_str(self):
         r1 = ClientResource('a3')
-        r1.set_lastmod('2012-01-11T01:02:03')
+        r1.lastmod='2012-01-11T01:02:03'
         self.assertEqual( str(r1), "<?xml version='1.0' encoding='UTF-8'?>\n<url><loc>a3</loc><lastmod>2012-01-11T01:02:03</lastmod></url>" )
 
     def test3b_str(self):
         r1 = ClientResource('3b',1234.1,9999,'ab54de')
         self.assertEqual( str(r1), "<?xml version='1.0' encoding='UTF-8'?>\n<url><loc>3b</loc><lastmod>1969-12-31T19:20:34.100000</lastmod><rs:size>9999</rs:size><rs:md5>ab54de</rs:md5></url>" )
+
+    def test4_bad_lastmod(self):
+        def setlastmod(r,v):
+            r.lastmod=v
+        r = ClientResource('4')
+        self.assertRaises( ValueError, setlastmod, r, "bad_lastmod" )
+        self.assertRaises( ValueError, setlastmod, r, "" )
+        self.assertRaises( ValueError, setlastmod, r, "2012-13-01" )
+        self.assertRaises( ValueError, setlastmod, r, "2012-12-32" )
+        self.assertRaises( ValueError, setlastmod, r, "2012-11-01T10:10" )
+        self.assertRaises( ValueError, setlastmod, r, "2012-11-01T10:10:60" )
+        self.assertRaises( ValueError, setlastmod, r, "2012-11-01T10:10:59.9x" )
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestClientResource)
