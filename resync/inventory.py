0a4e207ef4ad08a6150a0662993d170f20bfdfbc
Bernhard Haslhofer <bernhard.haslhofer@gmail.com>
2012-06-08 21:53:23 -0400

    refactored inventory; now added as source capability

--- resync/inventory.py
+++ resync/inventory.py
@@ -1,42 +1,32 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-inventory.py: A collection of inventory implementations.
+inventory.py: An inventory is a set of resources provided by some source.
 
 Created by Bernhard Haslhofer on 2012-04-26.
 Copyright 2012, ResourceSync.org. All rights reserved.
 """
 
-import tornado.web
-
 class Inventory(object):
-    """An inventory knows about a source and produces info about it contents"""
+    """An inventory knows about the source for getting its resources"""
     
     def __init__(self, source):
         self.source = source
+        
 
 class DynamicSiteMap(Inventory):
-    """A dynamic sitemap is generated on the fly"""
+    """An inventory that simply takes all resources from a source"""
 
     def __init__(self, source, config):
         super(DynamicSiteMap, self).__init__(source)
+        self.config = config
         print "\n*** Instantiated Dynamic SiteMap Generator ***"
         
     @property
-    def handlers(self):
-        return [(r"/sitemap.xml", 
-                DynamicSiteMapHandler,
-                dict(source = self.source))
-        ]
+    def resources(self):
+        """The resources are provided by the source"""
+        return self.source.resources
 
-class DynamicSiteMapHandler(tornado.web.RequestHandler):
-    """The HTTP request handler for the DynamicSiteMapInventory"""
-
-    def initialize(self, source):
-        self.source = source
-    
-    def get(self):
-        self.set_header("Content-Type", "application/xml")
-        self.render("sitemap.xml",
-                    resources = self.source.resources)
-    
+    def to_sitemap(self):
+        """Returns a Sitemap serialization of the inventory"""
+        return NotImplemented
