1b5ea8a21533986c0cad7f47b3a8b0ce04fbc463
Simeon Warner <simeon.warner@cornell.edu>
2012-07-15 16:23:36 -0400

    Add local file location

--- resync/resource_change.py
+++ resync/resource_change.py
@@ -1,20 +1,22 @@
 """Information about a web resource change
 
-Extension of a Resource with change information.
+Extension of a ResourceFile with change information.
 """
 
-from resource import Resource
+from resource_file import ResourceFile
 
-class ResourceChange(Resource):
-    __slots__=('uri', 'timestamp', 'size', 'md5', 'changeid', 'changetype')
+class ResourceChange(ResourceFile):
+    __slots__=('uri', 'timestamp', 'size', 'md5', 'file', 'changeid', 'changetype')
     
     def __init__(self, 
                  # Same interface as Resource
                  uri = None, timestamp = None, size = None, md5 = None, lastmod = None,
+                 # And ResourceFile
+                 resource = None, file = None,
                  # Alternative to create from an existing resource, plus extras
-                 resource = None, changeid = None, changetype = None):
-        """Create ResourceChange object from a Resource with optional change details"""
-        super(ResourceChange, self).__init__(uri=uri,timestamp=timestamp,size=size,md5=md5,lastmod=lastmod)
+                 changeid = None, changetype = None):
+        """Create ResourceChange object from a ResourceFile with optional change details"""
+        super(ResourceChange, self).__init__(uri=uri,timestamp=timestamp,size=size,md5=md5,lastmod=lastmod,file=file)
         # Create from a Resource?
         if (resource is not None):
             self.uri = resource.uri
@@ -27,5 +29,5 @@ class ResourceChange(Resource):
 
     def __str__(self):
         """Return a human readable string for this resource"""
-        return "[%s | %s | %d | %s | %s | %s ]" % (self.uri, self.lastmod, self.size,
-                                                   self.md5, self.changeid, self.changetype)
+        return "[%s | %s | %s | %s | %s | %s | %s ]" % (self.uri, str(self.lastmod), str(self.size),
+                                                        str(self.md5), str(self.file), str(self.changeid), str(self.changetype))
