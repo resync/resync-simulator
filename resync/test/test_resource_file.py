0cb7981c7b1555f388137ddfdb072a496ae3a391
Simeon Warner <simeon.warner@cornell.edu>
2012-07-16 15:26:31 -0400

    Avoid use of assertRegexpMatches to work with python 2.6

--- resync/test/test_resource_file.py
+++ resync/test/test_resource_file.py
@@ -1,8 +1,9 @@
 import unittest
+import re
 from resync.resource import Resource
 from resync.resource_file import ResourceFile
 
-class TestResource(unittest.TestCase):
+class TestResourceFile(unittest.TestCase):
 
     def test1_create(self):
         r = Resource('a', timestamp=1234)
@@ -11,7 +12,8 @@ class TestResource(unittest.TestCase):
         self.assertEqual( rf.timestamp, 1234)
         # So far these turn out equal
         self.assertEqual( r, rf )
-        self.assertRegexpMatches( str(rf), r"\[a | 1969" )
+        #py2.7: self.assertRegexpMatches( str(rf), r"\[a | 1969" )
+        self.assertTrue( re.match( r"\[a | 1969", str(rf) ) )
 
     def test2_create_with_file(self):
         r = Resource( 'a', timestamp=1234)
@@ -21,5 +23,5 @@ class TestResource(unittest.TestCase):
         self.assertEqual( r, rf )
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestClientResource)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestResourceFile)
     unittest.TextTestRunner(verbosity=2).run(suite)
