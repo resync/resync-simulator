fa2aa5d0fd7161db4db17e5261f26e46036d5d91
Bernhard Haslhofer <bernhard.haslhofer@cornell.edu>
2012-08-15 11:39:42 -0400

    added fileevent log

--- resync/event_log.py
+++ resync/event_log.py
@@ -7,14 +7,38 @@ Created by Bernhard Haslhofer on 2012-04-24.
 Copyright 2012, ResourceSync.org. All rights reserved.
 """
 
+import logging
+
 from observer import Observer
 
 class ConsoleEventLog(Observer):
     """This EventLog logs change events to the console"""
     
-    def __init__(self, source, config):
-        source.register_observer(self)
+    def __init__(self, observable, config=None):
+        observable.register_observer(self)
     
     def notify(self, change):
-        print "Eventlog: " + str(change)
+        print "Event: " + str(change)
+
+class FileEventLog(Observer):
+    """This EventLog logs change events to a log file"""
+
+    def __init__(self, observable, config=None):
+        observable.register_observer(self)
+        if config is None:
+            logfile = "source_events.log"
+        else:
+            logfile = config['filename']
+        logging.basicConfig(filename=logfile,
+                            filemode='w',
+                            format='%(asctime)s %(message)s',
+                            datefmt='%Y-%m-%dT%H:%M:%S',
+                            level=logging.INFO)
+                            
+    def notify(self, change):
+        logging.info(str(change))
+        
+
+def compare(server_log, client_log)
+
 
