d11cb622507741528e6ddefab7086ee69610ee8e
Simeon Warner <simeon.warner@cornell.edu>
2012-08-15 11:54:47 -0400

    Fix str formatting

--- resync/test/test_resource_file.py
+++ resync/test/test_resource_file.py
@@ -12,8 +12,8 @@ class TestResourceFile(unittest.TestCase):
         self.assertEqual( rf.timestamp, 1234)
         # So far these turn out equal
         self.assertEqual( r, rf )
-        #py2.7: self.assertRegexpMatches( str(rf), r"\[a | 1969" )
-        self.assertTrue( re.match( r"\[a | 1969", str(rf) ) )
+        #py2.7: self.assertRegexpMatches( str(rf), r"\[ a | 1969" )
+        self.assertTrue( re.match( r"\[ a | 1969", str(rf) ) )
 
     def test2_create_with_file(self):
         r = Resource( 'a', timestamp=1234)
