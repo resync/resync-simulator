f9988b705e94d0a3621885685d34dcab35d3e0e1
Bernhard Haslhofer <bernhard.haslhofer@cornell.edu>
2012-09-04 12:13:33 -0400

    changed state to set representation

--- analysis/analyze_logs.py
+++ analysis/analyze_logs.py
@@ -11,6 +11,7 @@ import re
 import ast
 import datetime
 import dateutil.parser
+import pprint
 
 
 class Resource(object):
@@ -23,80 +24,112 @@ class Resource(object):
         self.lastmod=lastmod
         self.size=size
         self.md5=md5
+    
+    def __eq__(self, other):
+        """Equality test for resources"""
+        return self.uri == other.uri
+        
+    def __hash__(self):
+        return hash(self.uri)
+        
+    def __str__(self):
+        return "[%s|%s|%s|%s]" % (self.uri, self.lastmod, self.size, self.md5)
+        
+    def __repr__(self):
+        return self.__str__()
 
 class LogAnalyzer(object):
     
     def __init__(self, source_log_file, destination_log_file):
         self.src_bootstrap_start = None
-        self.src_bootstrap_events = []
         self.src_simulation_start = None
-        self.src_change_events = []
         self.src_simulation_end = None
+        self.src_events = []
         if source_log_file is not None:
             self.parse_log_file(source_log_file)
         if destination_log_file is not None:
             "Doing nothing at the moment"
     
+    @property
+    def simulation_duration(self):
+        """Duration of the simulation"""
+        return self.src_simulation_end-self.src_simulation_start
+
+    @property
+    def src_bootstrap_events(self):
+        """The events that happended before the simulation start"""
+        return self.events_before(self.src_events, self.src_simulation_start)
+
+    @property
+    def src_simulation_events(self):
+        """The events that happened during the simulation"""
+        return [event for event in self.src_events
+                      if event['lastmod_dt'] >= self.src_simulation_start]
+    
+    def events_before(self, events, time):
+        """All events in events that happened before a certain time"""
+        return [event for event in events if event['lastmod_dt']<=time]
+    
     def parse_log_file(self, log_file):
         """Parses log files and returns a dictionary of extracted data"""
         print "Parsing %s ..." % log_file
-        simulation_started = False
-        events=[]
         for line in open(log_file, 'r'):
             log_entry = [entry.strip() for entry in line.strip().split("|")]
             if log_entry[3] == "Bootstrapping source...":
-                simulation_started = False
-                self.src_bootstrap_start = log_entry[0]
+                self.src_bootstrap_start = self.parse_datetime(log_entry[0])
             if log_entry[3] == "Starting simulation...":
-                simulation_started = True
-                self.src_bootstrap_events = events
-                events = []
-                self.src_simulation_start = log_entry[0]
+                self.src_simulation_start = self.parse_datetime(log_entry[0])
             if log_entry[3].find("Event: ") != -1:
                 event_dict_string = log_entry[3][len("Event: "):]
                 event_dict = ast.literal_eval(log_entry[3][len("Event: "):])
                 event_dt = self.parse_datetime(event_dict['lastmod'])
                 event_dict['lastmod_dt'] = event_dt
-                events.append(event_dict)
-        self.src_change_events = events
-        self.src_simulation_end = self.src_change_events[-1]['lastmod']
+                self.src_events.append(event_dict)
+        self.src_simulation_end = self.parse_datetime(
+                                        self.src_events[-1]['lastmod'])
         print "- Source Bootstrap time: %s" % self.src_bootstrap_start
         print "- Parsed %d bootstrap events" % (len(self.src_bootstrap_events))
         print "- Source Simulation start time: %s" % self.src_simulation_start
-        print "- Parsed %d simulated events" % (len(self.src_change_events))
+        print "- Parsed %d simulated events" % (len(self.src_events))
         print "- Source Simulation end time: %s" % self.src_simulation_end
-        self.duration = self.parse_datetime(self.src_simulation_end) - \
-                        self.parse_datetime(self.src_simulation_start)
-        print "- Total Simulation duration: %s" % self.duration
-                    
+        print "- Total Simulation duration: %s" % str(self.simulation_duration)
+    
     def compute_sync_accuracy(self, intervals=10):
         """Outputs synchronization accuracy at given intervals"""
-        interval_duration = self.duration.seconds / (intervals)
-        start_datetime = self.parse_datetime(self.src_simulation_start)
-        print "Time\t\t\t\tResources\te_created\te_updated\te_deleted"
+        interval_duration = self.simulation_duration.seconds / intervals
+        print "Time\t\t\t\tResources\te_created\te_updated\te_deleted\taccuracy"
         for interval in range(intervals+1):
             delta = interval_duration * interval
-            time = start_datetime + datetime.timedelta(0, delta)
+            time = self.src_simulation_start + datetime.timedelta(0, delta)
             src_state = self.compute_source_state(time)
             dst_state = self.compute_source_state(time) #TODO: impl dest state
-            # accuracy = set(src_state['resources'].values()) - \
-            #            set(dst_state['resources'].values())
-            # print accuracy
-            print "%s\t%d\t\t%d\t\t%d\t\t%d" % (time, 
+            
+            accuracy = len(src_state['resources']) - \
+                       len(dst_state['resources'])
+            print "%s\t%d\t\t%d\t\t%d\t\t%d\t\t%f" % (time,
                                     len(src_state['resources']),
                                     src_state['e_created'],
                                     src_state['e_updated'],
-                                    src_state['e_deleted'])
+                                    src_state['e_deleted'],
+                                    accuracy)
     
     def compute_source_state(self, time):
-        "Compute the resource state at a given point in time"
-        resources={}
-        for event in self.src_bootstrap_events: # bootstrap events
-            self.apply_event(resources, event)
-        events = [event for event in self.src_change_events # change events
-                            if event['lastmod_dt']<=time]
+        "Compute the set of resources at a given point in time"
+        resources=set()
+        events = self.events_before(self.src_events, time)
         for event in sorted(events, key=lambda event: event['lastmod_dt']):
-            self.apply_event(resources, event)
+            resource = Resource(uri=event['uri'], md5=event['md5'],
+                                size=event['size'],lastmod=event['lastmod'])
+            if event['changetype'] == "CREATED":
+                resources.add(resource)
+            elif event['changetype'] == "UPDATED":
+                resources.remove(resource)
+                resources.add(resource)
+            elif event['changetype'] == "DELETED":
+                resources.remove(resource)
+            else:
+                print "WARNING - Unknow changetype in event %s" % event
+        
         e_created = [event for event in events
                         if event['changetype']=='CREATED']
         e_updated = [event for event in events
@@ -109,30 +142,13 @@ class LogAnalyzer(object):
                  'e_deleted': len(e_deleted)}
         return state
 
+    
     # PRIVATE STUFF
     
     def parse_datetime(self, utc_datetime_string):
         """Parse a datetime object from a UTC string"""
         fmt = '%Y-%m-%dT%H:%M:%S.%fZ'
         return datetime.datetime.strptime(utc_datetime_string, fmt)
-    
-    def apply_event(self, resources, event):
-        """Applies a given event to a given list of resources"""
-        changetype = event['changetype']
-        resource = Resource(uri=event['uri'],
-                            md5=event['md5'],
-                            size=event['size'],
-                            lastmod=event['lastmod'])
-        if changetype == "CREATED":
-            resources[resource.uri] = resource
-        elif changetype == "UPDATED":
-            del resources[resource.uri]
-            resources[resource.uri] = resource
-        elif changetype == "DELETED":
-            del resources[resource.uri]
-        else:
-            print "WARNING - Unknow changetype in event %s" % event
-        return resources
 
 def main():
 
@@ -153,6 +169,18 @@ def main():
 
     analyzer = LogAnalyzer(args.source_log, args.destination_log)
     analyzer.compute_sync_accuracy()
+    
+    r1 = Resource(uri="uri1", md5="1234")
+    r2 = Resource(uri="uri2", md5="5678")
+    
+    r3 = Resource(uri="uri2", md5="5678")
+    r4 = Resource(uri="uri3", md5="9101")
+    
+    # s0 = set([r1,r2,r3,r4])
+    # s1 = set([r1,r2])
+    # s2 = set([r3,r4])
+    # 
+    # print s1-s2
 
 if __name__ == '__main__':
     main()
