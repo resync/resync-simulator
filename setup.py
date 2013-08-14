e7d60831aef55dec190281e54a7822e90e3668fc
Simeon Warner <simeon.warner@cornell.edu>
2013-08-14 15:49:37 -0400

    Fix version, add requires, add test_suite

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
+# setuptools used instead of distutils.core so that 
+# dependencies can be handled automatically
 
 setup(
     name='simulator',
-    version='0.5',
-    packages=['simulator'],
+    version='0.7',
+    packages=['simulator'], #FIXME - change to resync-simulator
+    scripts=['simulate-source'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     author='Bernhard Haslhofer',
     author_email='bernhard.haslhofer@univie.ac.at',
     long_description=open('README.md').read(),
+    install_requires=[
+        "resync>=0.9.3",
+        "tornado"
+    ],
+    test_suite="simulator.test",
 )
