b7c047b11fe3dd7bd362e4bb7d96c064eacb1df7
Simeon Warner <simeon.warner@cornell.edu>
2013-09-04 17:56:33 -0400

    Add classifiers

--- setup.py
+++ setup.py
@@ -7,10 +7,19 @@ setup(
     version='0.7',
     packages=['simulator'],
     scripts=['resync-simulator'],
+    classifiers=["Development Status :: 3 - Alpha",
+                 "Intended Audience :: Developers",
+                 "Operating System :: OS Independent", #is this true? know Linux & OS X ok
+                 "Programming Language :: Python",
+                 "Programming Language :: Python :: 2.7",
+                 "Topic :: Internet :: WWW/HTTP",
+                 "Environment :: Web Environment"],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     author='Bernhard Haslhofer',
     author_email='bernhard.haslhofer@univie.ac.at',
+    description='ResourceSync source simulator',
     long_description=open('README.md').read(),
+    url='http://github.com/resync/simulator',
     install_requires=[
         "resync>=0.9.3",
         "tornado"
