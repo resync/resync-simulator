f1280c8cefbadc12e4994f4c65f9a7f70b02a41b
Simeon Warner <simeon.warner@cornell.edu>
2016-05-06 19:48:00 -0400

    Add version number and --version option

--- setup.py
+++ setup.py
@@ -2,9 +2,22 @@ from setuptools import setup
 # setuptools used instead of distutils.core so that 
 # dependencies can be handled automatically
 
+# Extract version number from resync/_version.py. Here we
+# are very strict about the format of the version string
+# as an extra sanity check. (Thanks for comments in
+# http://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package )
+import re
+VERSIONFILE="simulator/_version.py"
+verfilestr = open(VERSIONFILE, "rt").read()
+match = re.search(r"^__version__ = '(\d\.\d.\d+(\.\d+)?)'", verfilestr, re.MULTILINE)
+if match:
+    version = match.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE))
+
 setup(
     name='resync-simulator',
-    version='0.7',
+    version=version,
     packages=['simulator'],
     package_data={'simulator': ['static/*','templates/*']},
     scripts=['resync-simulator'],
