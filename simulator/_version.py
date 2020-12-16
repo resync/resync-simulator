8446945a700edcd4f054ef411c42fd287e48af96
Simeon Warner <simeon.warner@cornell.edu>
2020-12-16 18:53:22 -0500

    Fix up version number and PyPI instructions

--- simulator/_version.py
+++ simulator/_version.py
@@ -1,6 +1,2 @@
 """This is the one place the version number for resync-simulator is stored."""
-#
-# Format: x.y.z where
-# x.y is spec version, see http://www.openarchives.org/rs/x.y/
-# z is incremented for revisions within that version, 1...
-__version__ = '1.0.4'
+__version__ = '1.1.0'
